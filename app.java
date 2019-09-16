b721976c7cb30ded90019ca82ad8b86f0c49a0a4
Ubuntu <ubuntu@ip-172-31-7-190.ap-south-1.compute.internal>
2019-09-16 05:40:54 +0000

    updated

--- app.java
+++ app.java
@@ -1,5 +1,5 @@
 [200~class Simple{  
 	    public static void main(String args[]){  
-		         System.out.println("Hello Divakar");  
+		         System.out.println("Hello Divakar...!");  
 			     }  
 }  
