eac9b495bcdd65d0abd5711000ad9d06973f4063
Divakar <divs18071994@gmail.com>
2019-09-18 05:22:46 +0000

    updatednow

--- app.java
+++ app.java
@@ -1,5 +1,6 @@
-[200~class Simple{  
+[200~class Simple{  
 	    public static void main(String args[]){  
-		         System.out.println("Hello Divakar Divakar...!");  
+		         System.out.println("Hello Divakar Divakar...!");
+		         System.out.println(Hello Divakar.);	 
 			     }  
 }  
