671145e2b47ac11bc041129fddaa6437928ad61f
Divs1994 <55476103+Divs1994@users.noreply.github.com>
2019-09-18 10:57:55 +0530

    Change from RR

--- app.java
+++ app.java
@@ -1,6 +1,7 @@
 [200~class Simple{  
 	    public static void main(String args[]){  
 		         System.out.println("Hello Divakar Divakar...!");
-		         System.out.println(Hello Divakar.);	 
+		         System.out.println(Hello Divakar.);
+		    	system.out.println(Hello)
 			     }  
 }  
